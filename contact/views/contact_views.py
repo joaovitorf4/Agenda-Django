a256aaa59e72e0ce34b10d4862de1c6c7fa0c60d
Joao Vitor <joaovitorfod5@gmail.com>
2023-05-08 10:23:42 -0300

    Configurando as URLs

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,18 +1,34 @@
-from contextlib import ContextDecorator
-
 from contact.models import Contact
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
 
     context = {
         'contacts': contacts,
     }
 
     return render(
-        request, 
+        request,
         'contact/index.html',
+        context,
+    )
+
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(
+        Contact,
+        pk=contact_id,
+        show=True,
+    )
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context,
     )

53e2d2e3d134a450fe90150a8171a20969b3c32d
Joao Vitor <joaovitorfod5@gmail.com>
2023-05-09 11:29:39 -0300

    Config Pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,18 @@
 from contact.models import Contact
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
     }
 
@@ -32,9 +37,14 @@ def search(request):
         ).\
         order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(

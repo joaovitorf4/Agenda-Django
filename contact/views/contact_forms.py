c615db8977181427dd057efd652f44a246b45730
Joao Vitor <joaovitorfod5@gmail.com>
2023-05-10 11:10:07 -0300

    Criando Form 2

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,6 +1,8 @@
+from typing import Any, Dict
+
 from contact.models import Contact
-from contact.views.contact_views import search
 from django import forms
+from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
@@ -10,32 +12,48 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = (
-            'first_name',
-            'last_name',
-            'phone',
+            'first_name', 'last_name', 'phone',
         )
-        
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro 2',
+                code='invalid'
+            )
+        )
+
+        return super().clean()
+
+
 def create(request):
     if request.method == 'POST':
         context = {
-            'form': ContactForm(data=request.POST),
+            'form': ContactForm(request.POST)
         }
 
         return render(
             request,
             'contact/create.html',
-            context,
+            context
         )
 
     context = {
-        'form': ContactForm(),
+        'form': ContactForm()
     }
 
     return render(
-            request,
-            'contact/create.html',
-            context,
+        request,
+        'contact/create.html',
+        context
     )
-
-
-    

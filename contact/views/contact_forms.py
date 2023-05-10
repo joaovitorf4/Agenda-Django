a8a4b8b48b7305b6b1c0119372d890dd6f9e8481
Joao Vitor <joaovitorfod5@gmail.com>
2023-05-10 11:03:25 -0300

    Criando Form dinâmico

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,16 +1,41 @@
 from contact.models import Contact
+from contact.views.contact_views import search
+from django import forms
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
 
-def create(request):
-    context = {
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name',
+            'last_name',
+            'phone',
+        )
         
+def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(data=request.POST),
+        }
+
+        return render(
+            request,
+            'contact/create.html',
+            context,
+        )
+
+    context = {
+        'form': ContactForm(),
     }
 
     return render(
-        request,
-        'contact/create.html',
-        context,
+            request,
+            'contact/create.html',
+            context,
     )
+
+
+    
